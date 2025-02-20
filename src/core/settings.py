373e4b9083bcd7793ef174ab2dd4677c36ea1c04
Youssef Bouryal <youssefbouryal02@gmail.com>
2025-02-20 02:34:30 +0100

    whitenoice

--- src/core/settings.py
+++ src/core/settings.py
@@ -49,13 +49,13 @@ MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "core.middlewares.CookieToAuthHeaderMiddleware",
     "django.middleware.security.SecurityMiddleware",
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = "core.urls"
