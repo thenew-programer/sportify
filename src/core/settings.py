e32589d500818a0bbc482ca7e12899eaaa5e0967
Youssef Bouryal <youssefbouryal02@gmail.com>
2025-02-20 02:23:28 +0100

    load static files

--- src/core/settings.py
+++ src/core/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "local-cdn"
+STATIC_ROOT = BASE_DIR / "static"
 
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
