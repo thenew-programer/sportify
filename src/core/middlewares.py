d00fc8cb24cbf3deed7b981098bef198bdcf8eb2
Youssef Bouryal <youssefbouryal02@gmail.com>
2025-02-20 01:04:04 +0100

    update middleware to bypass views that doesnt need authentication

--- src/core/middlewares.py
+++ src/core/middlewares.py
@@ -2,9 +2,18 @@ from django.utils.deprecation import MiddlewareMixin
 
 class CookieToAuthHeaderMiddleware(MiddlewareMixin):
     def process_request(self, request):
-        if (request.path == '/api/users/register' or
-            request.path == '/api/p/players'
-        ):
+        unauthenticated_paths = {
+            "/api/users/register",
+            "/api/auth/token",
+            "/api/auth/token/refresh",
+            "/api/auth/token/verify",
+            "/api/teams/",
+            "/api/matches/",
+            "/api/players/",
+            "/api/players/players-by-sport/",
+            "/api/players/players-by-team/"
+        }
+        if (request.path in unauthenticated_paths):
             return
         access_token = request.COOKIES.get("access_token")
         if access_token:
