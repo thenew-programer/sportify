4d3fee81c97d40b5c5313f1b68b7da3b94d37a6b
Youssef Bouryal <youssefbouryal02@gmail.com>
2025-02-20 01:02:54 +0100

    update urls

--- src/team/urls.py
+++ src/team/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import CreateTeam, RetrieveTeams
 
 urlpatterns = [
-    path("teams", RetrieveTeams.as_view()),
-    path("create", CreateTeam.as_view()),
+    path("", RetrieveTeams.as_view()),
+    path("create/", CreateTeam.as_view()),
 ]
