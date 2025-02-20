236100ec8067621c8c8c1970c08ed1f6f142a678
Youssef Bouryal <youssefbouryal02@gmail.com>
2025-02-20 01:02:43 +0100

    update urls

--- src/player/urls.py
+++ src/player/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from player.views import RegisterPlayer, RetrievePlayers, RetrievePlayersBySport, RetrievePlayersByTeam
 
 urlpatterns = [
-    path("players", RetrievePlayers.as_view()),
+    path("", RetrievePlayers.as_view()),
     path("create", RegisterPlayer.as_view()),
     path("players-by-sport/<str:sport_type>", RetrievePlayersBySport.as_view()),
     path("players-by-team/<str:team_name>", RetrievePlayersByTeam.as_view()),
